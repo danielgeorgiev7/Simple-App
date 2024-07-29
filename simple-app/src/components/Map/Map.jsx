9b14e40cd77c00db25335cc4f4799f1b5ae6c703
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-07-30 01:46:33 +0300

    Fix map issues

--- simple-app/src/components/Map/Map.jsx
+++ simple-app/src/components/Map/Map.jsx
@@ -1,16 +1,36 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.7.1/dist/images/";
+L.Icon.Default.mergeOptions({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
 
 function Map({ settings }) {
+  const {
+    position,
+    style,
+    zoom,
+    zoomControl,
+    doubleClickZoom,
+    scrollWheelZoom,
+    dragging,
+  } = settings;
+
   return (
     <MapContainer
-      center={settings.position}
-      style={settings.style}
-      zoom={settings.zoom}
-      zoomControl={settings.zoomControl}
-      doubleClickZoom={settings.doubleClickZoom}
-      scrollWheelZoom={settings.scrollWheelZoom}
-      dragging={settings.dragging}
+      center={position}
+      style={style}
+      zoom={zoom}
+      zoomControl={zoomControl}
+      doubleClickZoom={doubleClickZoom}
+      scrollWheelZoom={scrollWheelZoom}
+      dragging={dragging}
       className="map"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
